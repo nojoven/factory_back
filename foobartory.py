7ab06eac7d8db8a58d58f8a371eee369d7c31466
nojoven <work.cedricjoseph@gmail.com>
2022-04-24 18:19:06 +0200

    Add logic for mining, combining and selling

--- foobartory.py
+++ foobartory.py
@@ -1,14 +1,17 @@
-from robots import Bot
+import time
+import random
+from robots.robots import Bot
 
 
 class Factory:
-   current_time = 0
+   
    def __init__(self, robots_number=2, foo=0, bar=0, foobar=0, money=0):
       self.robots_number = robots_number
       self.bar = bar
       self.foo = foo
       self.foobar = foobar
       self.money = money
+      self.current_time = 0
       self.init_robot()
 
    def foo_mined(self):
@@ -20,26 +23,87 @@ class Factory:
    def combination_result(self, success):
       if success:
          self.foobar += 1
-         self.bar -= 1
-         self.foo -= 1
       else:
-         self.bar -= 1
+         self.foo += 1
    
    def init_robot(self):
       self.robots = [] 
       for i in range(self.robots_number):
-         self.robots.append(Bot())
+         self.robots.append(Bot(self.foo_mined, self.bar_mined, self.combination_result))
+      return self.robots
+
+   def sell_foobar(self):
+      self.foobars -= 5
+      print(f"Number of foobars sold: 5. {self.foobar} foobars left")
+      
+      self.money += 5
+
+   def buy_robot(self):
+      before_money = self.money
+      before_foos = self.foo
+      before_robots = self.robot
+      
+
+      if self.money > 3 and self.foo > 6:
+         print(f"Before payment: factory owns {before_money} €.")
+         print(f"Before payment: factory owns {before_foos} foos.")
+         print(f"Before payment: factory owns {before_robots} robots.")
+
+         self.money -= 3
+         self.foo -= 6
+         self.robots.append(Bot(self.foo_mined, self.bar_mined, self.combination_result))
+         print("Factory acquires 1 robot.")
+         print(f"Factory spent : {before_money - self.money}€.")
+         print(f"After payment : Factory owns {self.money} €.")
+
+         print(f"After payment : consummed {before_foos - self.foo} .")
+         print(f"Factory keeps : {self.foo} foos.")
+
+         print(f"Factory had")
+         print(f"")
+
+
+
+
 
    def work(self):
+      print("Production starts.")
+      robots = self.init_robot()
+   
       while self.robots_number < 30:
-         current_time += 1
-         break
+         
+         for robot in robots:
+            if not self.bar:
+               print("Not enough bar. Extraction will start.")
+               robot.mine_bar(fac.current_time)
+               continue
+            
+            if not self.foo:
+               print("Not enough foo. Extraction will start.")
+               robot.mine_foo(fac.current_time, 1)
+               continue
+            
+            if self.foo and self.bar:
+               "Starting the combination process."
+               robot.combine(fac.current_time, 2)
+               self.foo -= 1
+               self.bar -= 1 
+               continue
+            
+            if self.foobar >= 5:
+               before_money = self.money
+               print(f"Before sale: factory owns {before_money} €.")
+               fac.sell_foobar()
+               print(f"After sale: earned {self.money - before_money} €.")
+               print(f"Factory owns : {self.money} €.")
 
+         self.current_time += 1
 
-
+      
 
 
 
 
 if __name__ == "__main__":
-   work()
+   fac = Factory()
+   fac.work()
--- robots/robots.py
+++ robots/robots.py
@@ -2,33 +2,81 @@ import time
 import random
 
 
+class Task:
+   def __init__(self, completion_time, callback_method, task_result = None):
+      self.completion_time = completion_time
+      self.task_result = task_result
+      self.callback_method = callback_method
+
+   def perform(self):
+      if self.task_result:
+         print(f"I have finished {self.callback_method}.")
+         self.callback_method(self.task_result)
+      else:
+         print(f"I am going to start {self.callback_method}.")
+         self.callback_method()
+         print(f"I am performing {self.callback_method}.")
+
+
 
 
 class Bot:
    """This is the blueprint for our bots"""
-   def __init__(self, busy_until=0):
+
+   def __init__(
+         self, 
+         foo_mined, 
+         bar_mined,
+         combination_result,
+         busy_until=0):
       self.busy_until = busy_until
+      self.foo_mined = foo_mined
+      self.bar_mined = bar_mined
+      self.combination_result = combination_result
+      self.task = None
 
-   def move(self, current_time, execution_time=1):
-      if not self.is_free:
+   def is_same_task(self, callback_method):
+         return self.task is not None and self.task.callback_method == callback_method
+
+   def mine_foo(self, current_time, execution_time=1):
+      print("Received order : Mine foo !")
+      if self.busy_until > current_time:
+         print("Foo? I am busy.")
          return
-      self.busy_until = False
-   
-   def mine_foo(self, current_time, foo_mined, execution_time=0.25):
-      if not self.is_free:
+      self.perform_task(current_time)
+
+      self.busy_until = current_time + execution_time
+      completion_time = self.busy_until if self.is_same_task(self.foo_mined) else self.busy_until + 5
+      self.task = Task(completion_time, self.foo_mined)
+
+   def mine_bar(self,current_time, execution_time=(random.uniform(0.5, 2))):
+      if self.busy_until > current_time:
+         print("Bar? I am busy.")
          return
-      self.busy_until = False
+      self.perform_task(current_time)
 
-   def mine_bar(self,current_time, bar_mined, execution_time=random.randint(0.1, 0.4)):
-      if not self.is_free:
+      self.busy_until = current_time + execution_time
+      print(f"I will be busy until {self.busy_until}.")
+
+      completion_time = self.busy_until if self.is_same_task(self.bar_mined) else self.busy_until + 5
+      print(f"Required completion time = {completion_time}.")
+      
+      self.task = Task(completion_time, self.bar_mined)
+
+   def combine(self, current_time, execution_time=2):
+      if self.busy_until > current_time:
+         print("I am busy.")
          return
-      self.busy_until = False
+      self.perform_task(current_time)
 
-   def combine(self, current_time, combination_result, execution_time=0.4):
-      if not self.is_busy:
-         is_successful = random.randrange(100) < 60
+      self.busy_until = current_time + execution_time
+      print(f"I will be busy until {self.busy_until}.")
+      
+      completion_time = self.busy_until if self.is_same_task(self.combination_result) else self.busy_until + 5
+      print(f"Required completion time = {completion_time}.")
 
-      self.busy_until = False
+      self.task = Task(completion_time, self.combination_result, random.randrange(100) < 60)
 
-
-
+   def perform_task(self, current_time):
+      if self.task and self.task.completion_time > current_time :
+         self.task.perform()
